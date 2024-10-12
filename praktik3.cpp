54e26bd0a2da6258aa566f9b367c62e018fd4d5a
tamaasn <43458726+tamaasn@users.noreply.github.com>
2024-10-12 18:13:06 +0700

    Add files via upload

--- praktik3.cpp
+++ praktik3.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 struct Barang{
   unsigned int id;
@@ -46,7 +47,8 @@ struct Barang input_data(){
   scanf("%d",&harga);
   
   while(satuan > 3){
-    printf("1. Ampul\n2. Strip\n3. PCS");
+    printf("[INFO] Satuan?\n");
+    printf("1. Ampul\n2. Strip\n3. PCS\n");
     printf("Masukkan satuan : ");
     scanf("%d",&satuan);
   }
@@ -62,25 +64,50 @@ struct Barang input_data(){
 }
 
 void cetak_barang(struct Barang *barang){
+  printf("[INFO] Daftar barang\n");
   for(int i=0;i<id;i++){
     printf("[%d]. %s | %d | %s |\n",i+1,barang[i].nama,barang[i].harga,text_satuan[barang[i].satuan]);
   }
+  printf("[%d]. Batal\n", id+1);
 }
 
 int main(){
   struct Barang *barang = buat_node();
+  struct Barang temp;
+  FILE *file = fopen("data.bin","rb");
+  if (file == NULL){
+    printf("Gagal membaca bin file\n");
+  }
+  printf("File size : %ld/%ld\n",sizeof(file),sizeof(struct Barang));
+  while(fread(&temp,sizeof(struct Barang),1,file) == 1){
+    printf("Loading bin file...\n");
+    id++;
+    printf("Nama : %s\n",temp.nama);
+    isi_data(&barang,temp,id);
+  }
+  fclose(file);
+  printf("Done\n");
   int total_harga=0;
 
   unsigned int pilihan=0;
   while(pilihan < 4){
-    printf("Total harga = %d\n",total_harga);
+    #ifdef _WIN32
+    system("cls");
+    #elif _WIN64
+    system("cls");
+    #else
+    system("clear");
+    #endif
+
+    printf("\tTotal harga = %d\n",total_harga);
+    printf("Sunny Corp\n\n");
     printf("======================\n");
-    printf("1. Input data\n2. Pilih barang\n3. Beli\n4. Keluar\n"); 
+    printf("1. Input data\n2. Pilih barang\n3. Beli\n4. Simpan ke .bin\n5. Keluar\n"); 
     printf("Pilih : ");
     scanf("%d",&pilihan);
     switch(pilihan){
       case 1: {
-          struct Barang temp = input_data();
+          temp = input_data();
           isi_data(&barang,temp,id);
         }break;
       case 2: {
@@ -89,6 +116,7 @@ int main(){
           while(pilih > id || pilih < 1){
             printf("\nPilih : ");
             scanf("%d",&pilih);
+            if (pilih == id+1){ break; }
           }
           total_harga += barang[pilih-1].harga;
         }break;
@@ -98,13 +126,39 @@ int main(){
           scanf("%d",&input_harga);
           int kembalian=input_harga-total_harga;
           if (kembalian < 0){
-            printf("Uang kurang!!\n");
+            printf("[ERROR] Uang kurang!!\n");
+            printf("[INFO] Enter untuk lanjut!\n");
+            getchar();
+            getchar();
             break;
           }
-          printf("Kembalian : %d\n",kembalian);
+          printf("[INFO] Kembalian : %d\n",kembalian);
+          printf("[INFO] Enter untuk lanjut!\n");
+          getchar();
+          getchar();
           total_harga=0;
         }break;
       case 4: {
+            size_t count = id;
+            printf("Count : %ld\n",count);
+            file = fopen("data.bin","wb");
+            if (file == NULL){
+              printf("Gagal lihat file!\n");
+              break;
+            }
+
+            for(int i=0;i<count;i++){
+              size_t written = fwrite(&barang[i],sizeof(struct Barang),1,file);
+              if (written != 1){
+                printf("Gagal menyimpan file\n");
+                break;
+              }
+              printf("Data sukses disimpan!!\n");
+            }
+            fclose(file);
+          }
+          break;
+      case 5: {
           pilihan=5;
         }break;
       default:

192562234acff7a5df26c22087c7e120fcd0fe59
tamaasn <43458726+tamaasn@users.noreply.github.com>
2024-10-17 08:24:16 +0700

    Create reverse_string.c

---
+++ praktik_4/reverse_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_length(char text[] , int len){
+  int total=0;
+  for(int i=0;i<len && text[i] != '\0';i++){
+    total++;
+  }
+  //printf("total : %d\n",total);
+  return total;
+}
+
+char *reverse(char nama[] , int len){
+  char *result = (char*)malloc(sizeof(char) * len);
+  for(int i=0;i<len;i++){
+    char temp=nama[i];
+    result[i] = nama[(len-1)-i];
+    result[(len-1)-i] = temp;
+  }
+
+  return result;
+}
+
+int main(){
+  char nama[] = "Muhammad Akmal Sani Pratama\0";
+  char nim[] = "23106050036\0";
+  char asal[] = "Banguntapan, Bantul, Yogyakarta\0";
+  char *result1 = reverse(nama,get_length(nama,sizeof(nama)));
+  char *result2 = reverse(nim,get_length(nim,sizeof(nim)));
+  char *result3 = reverse(asal,get_length(asal,sizeof(asal)));
+  printf("Sebelum : %s\nSesudah : %s\n============\n",nama,result1);
+  printf("Sebelum : %s\nSesudah : %s\n============\n",nim,result2);
+  printf("Sebelum : %s\nSesudah : %s\n============\n",asal,result3);
+
+  free(result1);
+  free(result2);
+  free(result3);
+  return 0;
+}
